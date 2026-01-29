e7d8edc02e6cfa80ee8fc4e7da043f7a17f482f0
alejandrocasillasmoreno <alejandrocasillas.24@campuscamara.es>
2026-01-29 12:06:00 +0100

    Modificación

--- backend/Task.js
+++ backend/Task.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-  titulo: { type: String, required: true }, // [cite: 28]
-  tecnologia: { type: String },             // [cite: 29]
+  titulo: { type: String, required: true }, 
+  tecnologia: { type: String },             
   estado: { 
     type: String, 
     enum: ['pending', 'done'], 
     default: 'pending' 
-  },                                        // [cite: 30]
-  fecha: { type: Date, default: Date.now }   // [cite: 30]
+  },                                        
+  fecha: { type: Date, default: Date.now }   
 });
+
 
 module.exports = mongoose.model('Task', taskSchema);
